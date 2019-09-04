32950ea7cc36f27a84159f13be688474c22042c3
Zharif.Hadi @ 123RF <zharif.hadi@123rf.com>
2019-09-04 20:40:20 +0800

    Initial Commit

---
+++ ContactApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by M. Zharif Hadi M. Khairuddin on 04/09/2019.
+//  Copyright © 2019 M. Zharif Hadi M. Khairuddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
